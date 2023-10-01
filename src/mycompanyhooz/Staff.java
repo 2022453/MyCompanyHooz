4ebb7975edda78f39dd474d5ab5546c5c0ad6968
2022453 <2022453@student.cct.ie>
2023-10-01 19:23:32 +0100

    Setter and Getter
    Added the setter and getter to it.

--- src/mycompanyhooz/Staff.java
+++ src/mycompanyhooz/Staff.java
@@ -19,6 +19,30 @@ public class Staff {
         this.position = position;
         this.employeeID = employeeID;
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+   
     
 }
